bcc9faafdb1801acbf236d855d2d2f5d1a8ac5ff
ammadaltaf <ammadaltaf005@gmail.com>
2024-06-21 10:37:54 +0500

    Some Changes in Main Repo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,7 @@ class PostController extends Controller
     public function index()
     {
         // Hello world
+        // some changes in main repe
         // $post = Post::get();
         // return response()->json(['message'=>'List of Posts','posts'=>$post],200);
         $post = Post::find(1);
